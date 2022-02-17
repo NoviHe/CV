5956f7437b5d06865589d34ef2a37a3ec007680b
Novi Herlambang <claywhoami6@gmail.com>
2022-02-17 18:44:04 +0700

    add github link

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -2,52 +2,62 @@ const project = [
     {
         nama: "Laravel Pengaduan App",
         img: "./assets/img/portfolio/pengaduan.PNG",
-        link: "http://pengaduan.novimovie.xyz/"
+        link: "http://pengaduan.novimovie.xyz/",
+        github:"https://github.com/NoviHe/CiganjengDamai-pengaduan-app"
     },
     {
         nama: "Kotlin CEXCANGER App",
         img: "./assets/img/portfolio/android-kotlin_11zon.png",
-        link: "#"
+        link: "#",
+        github:"#"
     },
     {
         nama: "Laravel Sistem Informasi Data Asset",
         img: "./assets/img/portfolio/laravel-sida.PNG",
-        link: "#"
+        link: "#",
+        github:"#"
     },
     {
         nama: "PHP Lelang Ikan (Dataminer Algoritma Apriori)",
         img: "./assets/img/portfolio/lelang_11zon.png",
-        link: "http://lelang-ikan.novimovie.xyz/"
+        link: "http://lelang-ikan.novimovie.xyz/",
+        github:"#"
     },
     {
         nama: "PHP Penggajian App",
         img: "./assets/img/portfolio/penggajian.PNG",
-        link: "http://penggajian.novimovie.xyz/"
+        link: "http://penggajian.novimovie.xyz/",
+        github:"#"
     },
     {
         nama: "IoT Smart Home Project",
         img: "./assets/img/portfolio/smarthome.jpg",
-        link: "#"
+        link: "#",
+        github:"#"
     },
     {
         nama: "Laravel Sistem Pembuat Keputusan (SPK) App",
         img: "./assets/img/portfolio/spk.PNG",
-        link: "#"
+        link: "#",
+        github:"#"
     },
     {
         nama: "Develop Project T-Hypestor",
         img: "./assets/img/portfolio/t-store.jpg",
-        link: "#"
+        link: "#",
+        github:"#"
     },
     {
         nama: "Kotlin Github User App",
         img: "./assets/img/portfolio/github-user.png",
-        link: "#"
+        link: "#",
+        github:"https://github.com/NoviHe/githubUserApp_kotlin"
     },
     {
         nama: "Develop Project TBL-Cargo",
         img: "./assets/img/portfolio/tbl.png",
-        link: "#"
+        link: "#",
+        github:"#"
     }
 ];
 project.forEach((i) => {
@@ -57,7 +67,13 @@ project.forEach((i) => {
             <div class="card-body text-center">
                 <img src="${i.img}" class="w-100 img-thumbnail" alt="portfolio">
                 <h3 class="text-capitalize fs-5 my-2">${i.nama}</h3>
-                <a href="${i.link}" class="btn btn-outline-primary" target="_blank">Demo <i class="fa fa-arrow-right"></i></a>
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="${i.link}" class="btn btn-outline-primary" target="_blank">Demo <i class="fa fa-arrow-right"></i></a>
+                        
+                        <a href="${i.github}" class="btn btn-outline-dark" target="_blank">GitHub <i class="fa fa-arrow-right"></i></a>
+                    </div>
+                </div>
             </div>
         </div>
     </li>
